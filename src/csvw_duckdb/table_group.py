002fa8dac14ed4728b7722dd11ed38874ac9d377
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-12-19 11:22:50 +0000

    Convert to DuckDB syntax

--- src/csvw_duckdb/table_group.py
+++ src/csvw_duckdb/table_group.py
@@ -1,9 +1,10 @@
 import csvw.db
+import csvw_duckdb.table
 
 
 class TableGroup:
     """
-    A a group of tables, which are data structure made of a collection of rows and columns.
+    A group of tables, which are data structure made of a collection of rows and columns.
     https://www.w3.org/ns/csvw#class-definitions
     """
 
@@ -14,7 +15,17 @@ class TableGroup:
         self.path = path
         self.table_group = csvw.TableGroup.from_file(path)
         self.database = csvw.db.Database(self.table_group)
+        "SQLite database"
+
+    @property
+    def tables(self):
+        for table in self.database.tables:
+            yield csvw_duckdb.table.Table(table)
 
     def iter_sql(self):
-        for table in self.database.tables:
-            yield table.sql(translate=self.database.translate) + ';'
+        """
+        Get the SQL CREATE TABLE statement for each table in the database.
+        """
+        # https://csvw.readthedocs.io/en/latest/db.html
+        for table in self.tables:
+            yield table.sql(translate=self.database.translate)
