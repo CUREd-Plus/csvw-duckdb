586844cde4bb2d92334087173da3f6484e16c3f8
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-12-19 10:55:38 +0000

    Implement basic functionality

--- src/csvw_duckdb/__main__.py
+++ src/csvw_duckdb/__main__.py
@@ -1,9 +1,9 @@
 import argparse
-import json
 import logging
 from pathlib import Path
 
-from .table import Table
+from . import __version__
+from .table_group import TableGroup
 
 logger = logging.getLogger(__name__)
 
@@ -34,23 +34,24 @@ to DuckDB SQL data types https://duckdb.org/docs/sql/data_types/overview.html
 def get_args():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("csvw_path", type=Path, help="CSVW file")
+    parser.add_argument(
+        "--loglevel",
+        "-l",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="WARNING",
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"csvw-duckdb {__version__}"
+    )
     return parser.parse_args()
 
 
 def main():
     args = get_args()
+    logging.basicConfig(level=args.loglevel)
 
-    with args.csvw_path.open() as file:
-        csvw = json.load(file)
-        logger.info("Loaded %s", file.name)
-
-        dialect = csvw.get("dialect", dict())
-
-        for table in csvw["tables"]:
-            print(table)
-
-            sql = Table(table).to_sql()
-            print(sql)
+    for sql in TableGroup(args.csvw_path).iter_sql():
+        print(sql)
 
 
 if __name__ == "__main__":
---
+++ src/csvw_duckdb/table_group.py
@@ -0,0 +1,20 @@
+import csvw.db
+
+
+class TableGroup:
+    """
+    A a group of tables, which are data structure made of a collection of rows and columns.
+    https://www.w3.org/ns/csvw#class-definitions
+    """
+
+    def __init__(self, path):
+        """
+        :param table: CSVW table definition https://www.w3.org/ns/csvw#class-definitions
+        """
+        self.path = path
+        self.table_group = csvw.TableGroup.from_file(path)
+        self.database = csvw.db.Database(self.table_group)
+
+    def iter_sql(self):
+        for table in self.database.tables:
+            yield table.sql(translate=self.database.translate)
